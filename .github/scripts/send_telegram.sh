0d444b6ffcf1a194dfdd75492bb54278ebeec544
abdulrazaq21 <balawii1199@gmail.com>
2026-02-28 09:08:17 +0300

    Update send_telegram.sh

--- .github/scripts/send_telegram.sh
+++ .github/scripts/send_telegram.sh
@@ -3,4 +3,4 @@
 
 curl -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
   -d "chat_id=$TELEGRAM_CHAT_ID" \
-  -d "text=رسالة تجريبية من GitHub ✅"
+  -d "text=لا اله الا انت سبحانك اني كنت من الظالمين"
